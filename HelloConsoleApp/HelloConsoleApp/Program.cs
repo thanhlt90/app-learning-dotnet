dd2174942963db9383ee2e5e677c0bcc1803bbaf
thanhlt90 <thanhtl90@gmail.com>
2022-06-13 09:11:19 +0700

    Init helloConsoleApp
    - Init helloconsoleapp solution
    - Create helloconsoleapp project
    - Init debugger on vscode for console app

---
+++ HelloConsoleApp/HelloConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using Humanizer;
+
+namespace HelloDotnet
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello dotnet {args}");
+            HumanizeQuantities();
+            int a = 10;
+            int b = 20;
+            int c = a + b;
+            Console.WriteLine("Sum is equal = ", c);
+        }
+
+
+        static void HumanizeQuantities()
+        {
+            Console.WriteLine("case".ToQuantity(0));
+            Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
+            Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
+            Console.WriteLine(TimeSpan.FromDays(1).Humanize());
+            Console.WriteLine(TimeSpan.FromDays(16).Humanize());
+        }
+    }
+}
